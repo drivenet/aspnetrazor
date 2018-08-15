22ae62d6dd05ababfebeab915672b3e5dfb53c89
Aristarkh Zagorodnikov <xm@drive.net>
2018-08-15 15:55:56 +0300

    Refresh for VS2017 15.8

--- src/Package/RazorVersionDetector.cs
+++ src/Package/RazorVersionDetector.cs
@@ -3,93 +3,46 @@ using System.Collections.Concurrent;
 using System.Reflection;
 
 using Microsoft.Html.Editor.ContainedLanguage.Razor.Def;
-using Microsoft.VisualStudio.Html.Package.Workspace;
 using Microsoft.VisualStudio.Text;
-using Microsoft.Web.Editor.EditorHelpers;
 
 namespace AspNet.Razor_vHalfNext
 {
     internal sealed class RazorVersionDetector : IRazorVersionDetector
     {
-        private readonly IRazorVersionDetector _inner;
+        private static readonly Version OldVersion = new Version(3, 0, 0, 0);
 
-        private static readonly Lazy<MethodInfo> _getExplicitWebPagesVersionMethod = new Lazy<MethodInfo>(CreateGetExplicitWebPagesVersionMethod);
+        private static readonly Version NewVersion = new Version(SupportedRuntime.Version);
+
+        private readonly IRazorVersionDetector _inner;
 
         private static readonly ConcurrentDictionary<Type, MethodInfo> _setCachedVersionMethodCache = new ConcurrentDictionary<Type, MethodInfo>();
 
         public RazorVersionDetector(IRazorVersionDetector razorVersionDetector)
         {
-            _inner = razorVersionDetector;
+            _inner = razorVersionDetector ?? throw new ArgumentNullException(nameof(razorVersionDetector));
         }
-
-        private const string RazorSupportedRuntimeVersion2 = "RazorSupportedRuntimeVersion2";
 
         public Version GetVersion(ITextBuffer textBuffer)
         {
-            Version version = null;
-            if (textBuffer != null)
+            var version = _inner.GetVersion(textBuffer);
+            if (version == OldVersion)
             {
-                bool fromCache = false;
-                foreach (var buffer in textBuffer.GetContributingBuffers())
+                version = NewVersion;
+                var innerType = _inner.GetType();
+                var setCachedVersionMethod = _setCachedVersionMethodCache.GetOrAdd(innerType,
+                    type => type.GetMethod("SetCachedVersion", BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(ITextBuffer), typeof(Version) }, null));
+                if (setCachedVersionMethod != null)
                 {
-                    if (buffer.Properties.TryGetProperty(RazorSupportedRuntimeVersion2, out version))
+                    try
                     {
-                        fromCache = true;
-                        break;
+                        setCachedVersionMethod.Invoke(_inner, new object[] { textBuffer, version });
                     }
-                }
-                if (!fromCache)
-                {
-                    var getExplicitWebPagesVersionMethod = _getExplicitWebPagesVersionMethod.Value;
-                    if (getExplicitWebPagesVersionMethod != null)
+                    catch (TargetInvocationException)
                     {
-                        var fileName = textBuffer.GetFileName();
-                        var projectData = Microsoft.VisualStudio.Html.Package.Project.ProjectData.FromFileNameAndPath(fileName);
-                        if (projectData != null)
-                        {
-                            var absolutePath = VsWebUrl.CreateAppUrl(projectData.Hierarchy, projectData.ItemID).AbsolutePath;
-                            try
-                            {
-                                version = (Version)getExplicitWebPagesVersionMethod.Invoke(null, new object[] { absolutePath });
-                            }
-                            catch
-                            {
-                            }
-                        }
                     }
-                    textBuffer.Properties[RazorSupportedRuntimeVersion2] = version;
-                }
-                if (version != null)
-                {
-                    if (_inner != null)
-                    {
-                        var innerType = _inner.GetType();
-                        var setCachedVersionMethod = _setCachedVersionMethodCache.GetOrAdd(innerType,
-                            type => type.GetMethod("SetCachedVersion", BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(ITextBuffer), typeof(Version) }, null));
-                        if (setCachedVersionMethod != null)
-                        {
-                            try
-                            {
-                                setCachedVersionMethod.Invoke(_inner, new object[] { textBuffer, version });
-                            }
-                            catch (TargetInvocationException)
-                            {
-                            }
-                        }
-                    }
-                    return version;
                 }
             }
-            if (_inner != null)
-                version = _inner.GetVersion(textBuffer);
             return version;
-        }
-
-        private static MethodInfo CreateGetExplicitWebPagesVersionMethod()
-        {
-            var type = Type.GetType("System.Web.WebPages.Deployment.WebPagesDeployment, System.Web.WebPages.Deployment", false);
-            var method = type?.GetMethod("GetExplicitWebPagesVersion", BindingFlags.Static | BindingFlags.Public, null, new[] { typeof(string) }, null);
-            return method;
         }
     }
 }
