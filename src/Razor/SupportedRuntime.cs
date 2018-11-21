f1ac704f25770c085bea955d5638ee1582bba106
Aristarkh Zagorodnikov <xm@drive.net>
2018-11-21 23:38:23 +0300

    Refresh for VS 15.9

--- src/Razor/SupportedRuntime.cs
+++ src/Razor/SupportedRuntime.cs
@@ -2,8 +2,6 @@ namespace AspNet.Razor_vHalfNext
 {
 	public static class SupportedRuntime
 	{
-		public const string Version = "3.3.0.0";
-
-		public const string MvcVersion = "5.5.0.0";
+		public const string Version = "3.10.0.0";
 	}
 }
